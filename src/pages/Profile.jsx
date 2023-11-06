cb2acd6fb391373ba0fc11beb40f0ff72fe7d274
aliyousafzai183 <warisallah923@gmail.com>
2023-11-06 20:14:15 +0500

    disabled email field cannot changed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -182,6 +182,7 @@ const Profile = () => {
                         </label>
                         <input
                           type='email'
+                          disabled={true}
                           value={newData.email || ''}
                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           placeholder="mtos@gmail.com"
