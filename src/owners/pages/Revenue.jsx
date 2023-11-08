a1cd3f6d52c3fb398f46704c7571a50742ab8b40
aliyousafzai183 <warisallah923@gmail.com>
2023-11-08 17:08:32 +0500

    made some changes in owner

--- src/owners/pages/Revenue.jsx
+++ src/owners/pages/Revenue.jsx
@@ -51,17 +51,6 @@ const Revenue = () => {
     }
   });
 
-  // useEffect(() => {
-  //   if (token) {
-  //     getSubOwnerById(token, (result) => {
-  //       if (result.isSuccess) {
-  //         setUserProfile(result.data);
-  //         console.log(result.data);
-  //       }
-  //     })
-  //   }
-  // }, []);
-
   if (token === null) {
     navigate('/login');
   }
@@ -105,28 +94,8 @@ const Revenue = () => {
           <div>
             {themeSettings && <ThemeSettings />}
 
-            <div className="mt-24">
-              <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-
-                <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-                  {earningData.map((item) => (
-                    <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl">
-                      <button
-                        type="button"
-                        style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-                        className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
-                      >
-                        {item.icon}
-                      </button>
-                      <p className="mt-3">
-                        <span className="text-lg font-semibold">{item.amount}</span>
-                      </p>
-                      <p className="text-sm text-gray-400 mt-1">{item.title}</p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-
+            <div className="mt-24 mx-10">
+              <Header category='Insights' title='All Time Revenue' />
               <div className="flex gap-10 flex-wrap justify-center">
                 <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
                   <div className="flex justify-between">
--- src/pages/Subscriptions.jsx
+++ src/pages/Subscriptions.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 import { IoMdAdd, IoMdRemove } from 'react-icons/io';
-import { dropdownData } from '../data/dummy';
+import { recentTransactions } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
@@ -165,7 +165,39 @@ const Subscriptions = () => {
 
                   {
                     userProfile.subscriptions ?
-                      <div></div>
+                      <div className="flex gap-10 m-4 flex-wrap justify-center">
+                        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl">
+                          <div className="flex justify-between items-center gap-2">
+                            <p className="text-xl font-semibold">Previous Subscriptions</p>
+                          </div>
+                          <div className="mt-10 w-72 md:w-400">
+                            {recentTransactions.map((item) => (
+                              <div key={item.title} className="flex w-100 justify-between mt-4">
+                                <div className="flex gap-4">
+                                  <button
+                                    type="button"
+                                    style={{
+                                      color: item.iconColor,
+                                      backgroundColor: item.iconBg,
+                                    }}
+                                    className="text-2xl rounded-lg p-4 hover:drop-shadow-xl"
+                                  >
+                                    {item.icon}
+                                  </button>
+                                  <div>
+                                    <p className="text-md font-semibold">{item.title}</p>
+                                    <p className="text-sm text-gray-400">{item.desc}</p>
+                                  </div>
+                                </div>
+                                <p className={`text-${item.pcColor}`}>{item.amount}</p>
+                              </div>
+                            ))}
+                          </div>
+                          <div className="flex justify-between items-center mt-5 border-t-1 border-color">
+                            <p className="text-gray-400 text-sm">36 Previous Transactions</p>
+                          </div>
+                        </div>
+                      </div>
                       :
                       <div>
                         <section id="pricing" class="pricing-content section-padding justify ">
