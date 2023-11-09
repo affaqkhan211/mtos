e1dc08fb7df1c3aa77691b92fea44ff60ca3756b
aliyousafzai183 <warisallah923@gmail.com>
2023-11-09 16:48:22 +0500

    hide api key from reading

--- src/mtos/db/config.js
+++ src/mtos/db/config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-CaJ_FgXV1jGkaLnSnioiEsLte5e2SYY",
-  authDomain: "mtos-de3a5.firebaseapp.com",
-  projectId: "mtos-de3a5",
-  storageBucket: "mtos-de3a5.appspot.com",
-  messagingSenderId: "371277872017",
-  appId: "1:371277872017:web:103440044003ddc384b830",
-  measurementId: "G-J70XHG4PD9"
+  apiKey: process.env.API_KEY,
+  authDomain: process.en.AUTH_DOMAIN,
+  projectId:  process.en.PROJECT_ID,
+  storageBucket:  process.en.STORAGE_BUCKET,
+  messagingSenderId:  process.en.MESSAGING_SENDER_ID,
+  appId:  process.en.APP_ID,
+  measurementId:  process.en.MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
