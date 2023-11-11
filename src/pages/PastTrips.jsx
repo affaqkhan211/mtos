d9a8dad3c6f2616f4f68540aa515a4629a26e44c
aliyousafzai183 <warisallah923@gmail.com>
2023-11-11 14:21:07 +0500

    Updated Side bar

--- src/pages/PastTrips.jsx
+++ src/pages/PastTrips.jsx
@@ -85,7 +85,7 @@ const Trips = () => {
             <Sidebar />
           </div>
         ) : (
-          <div className="w-0 dark:bg-secondary-dark-bg">
+          <div className="w-20 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
             <Sidebar />
           </div>
         )}
@@ -93,7 +93,7 @@ const Trips = () => {
           className={
             activeMenu
               ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
-              : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+              : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 md:ml-20"
           }
         >
           <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
